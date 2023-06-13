2b0f120ebd2bd7f3a737e3c306fdd0cf403ef1a6
Sviatoslav <sviatoslavnazin@gmail.com>
2023-06-14 02:10:13 +0800

    add github actions

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  '{apps,libs}/**/*.{ts,tsx}': (files) => {
-    return `nx affected --target=typecheck --files=${files.join(',')}`;
-  },
   '{apps,libs}/**/*.{js,ts,jsx,tsx,json}': [
-    (files) => `nx affected:lint --files=${files.join(',')}`,
     (files) => `nx format:write --files=${files.join(',')}`,
+    (files) => `nx affected -t lint --files=${files.join(',')}`,
   ],
 };
